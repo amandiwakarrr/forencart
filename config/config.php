9c3e3f490c99319fa0c6e655946d3203d9939f7b
Aman Diwakar <157607718+amandiwakar86@users.noreply.github.com>
2026-03-31 21:04:45 +0530

    swithch local to global

--- config/config.php
+++ config/config.php
@@ -1,5 +1,8 @@
 <?php
 $base_url = "http://" . $_SERVER['HTTP_HOST'] . "/forencart/"; // for local server Xamp
-// $base_url = "https://" . $_SERVER['HTTP_HOST'] . "/forencart/"; // for ngrok when it is live its https which is globaly accessible on the internet
+
+// for ngrok when it is live its https which is globaly accessible on the internet
+
+// $base_url = "https://" . $_SERVER['HTTP_HOST'] . "/forencart/"; 
 ?>
 
