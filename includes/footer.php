f05703866eaf88faa5e36a7cafaf17167abfec8c
Aman Diwakar <157607718+amandiwakar86@users.noreply.github.com>
2026-04-04 19:31:37 +0530

    add more links in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
-<?php
-require_once 'header.php';
-?>
+<?php require_once 'header.php'; ?>
 
+<!-- FONT AWESOME (for icons) -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
 <footer class="site-footer">
 
@@ -9,11 +9,18 @@ require_once 'header.php';
 
         <!-- BRAND -->
         <div class="footer-box">
-            <h3>Foren<span>Cart</span></h3>
-            <p>Your one-stop destination for all category products.</p>
+            <h2 class="logo">Foren<span>Cart</span></h2>
+            <p>Your one-stop destination for all category products at the best prices.</p>
+
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            </div>
         </div>
 
-        <!-- LINKS -->
+        <!-- QUICK LINKS -->
         <div class="footer-box">
             <h4>Quick Links</h4>
             <ul>
@@ -21,6 +28,7 @@ require_once 'header.php';
                 <li><a href="<?php echo $base_url; ?>pages/shop.php">Shop</a></li>
                 <li><a href="<?php echo $base_url; ?>pages/cart.php">Cart</a></li>
                 <li><a href="<?php echo $base_url; ?>pages/contact.php">Contact</a></li>
+                <li><a href="#">FAQs</a></li>
             </ul>
         </div>
 
@@ -31,18 +39,41 @@ require_once 'header.php';
                 <li><a href="<?php echo $base_url; ?>pages/shop.php?category=electronics">Electronics</a></li>
                 <li><a href="<?php echo $base_url; ?>pages/shop.php?category=fashion">Fashion</a></li>
                 <li><a href="<?php echo $base_url; ?>pages/shop.php?category=jewellery">Jewellery</a></li>
+                <li><a href="<?php echo $base_url; ?>pages/shop.php?category=beauty">Beauty</a></li>
+                <li><a href="<?php echo $base_url; ?>pages/shop.php?category=toys">Toys</a></li>
+                <li><a href="<?php echo $base_url; ?>pages/shop.php?category=sports">Sports</a></li>
             </ul>
+        </div>
+
+        <!-- NEWSLETTER -->
+        <div class="footer-box">
+            <h4>Subscribe</h4>
+            <p>Get updates on latest offers & products.</p>
+            <form class="newsletter">
+                <input type="email" placeholder="Enter your email" required>
+                <button type="submit">Subscribe</button>
+            </form>
         </div>
 
         <!-- CONTACT -->
         <div class="footer-box">
             <h4>Contact</h4>
-            <p>Email: support@forencart.com</p>
-            <p>Phone: +91-XXXXXXXXXX</p>
+            <p>Email: contact@forencart.com</p>
+            <p>Phone: +91-8630750297</p>
+            <p>Location: Firozabad, India</p>
         </div>
 
     </div>
 
+    <!-- PAYMENT METHODS -->
+    <div class="payment-methods">
+        <img src="https://img.icons8.com/color/48/visa.png"/>
+        <img src="https://img.icons8.com/color/48/mastercard.png"/>
+        <img src="https://img.icons8.com/color/48/paypal.png"/>
+        <img src="https://img.icons8.com/color/48/google-pay.png"/>
+    </div>
+
+    <!-- BOTTOM -->
     <div class="footer-bottom">
         © <?php echo date("Y"); ?> ForenCart. All rights reserved.
     </div>
--- pages/shop.php
+++ pages/shop.php
@@ -50,9 +50,9 @@ $categories = mysqli_query(
             <!-- PRICE -->
             <div class="filter-box">
                 <h4>Price</h4>
-                <input style="margin:0px 4px 0px 0px;" type="number" id="minPrice" placeholder="Min ₹">
-                <input type="number" id="maxPrice" placeholder="Max ₹">
-                <button id="applyPrice">Apply</button>
+                <input style="margin:0px 4px 0px 0px; width:50px;" type="number" id="minPrice" placeholder="Min ₹">
+                <input style="margin:0px 4px 0px 0px; width:50px;" type="number" id="maxPrice" placeholder="Max ₹">
+                <button style="padding:0px 5px; background:#fff; border:1px solid black; border-radius:4px;" id="applyPrice">Apply</button>
             </div>
 
         </div>
