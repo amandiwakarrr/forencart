138f0b2a2edb2e9cd847b80c313971a9a5ec1f58
Aman Diwakar <amandiwakarfghp@gmail.com>
2026-04-18 21:34:19 +0530

    change bd name and https to http for local host

--- includes/db.php
+++ includes/db.php
@@ -2,9 +2,9 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "forencart_db";
+$db   = "forencart";
 
-$conn = mysqli_connect("localhost", "root", "", "forencart_db", 3306);
+$conn = mysqli_connect("localhost", "root", "", "forencart", 3306);
 
 if (!$conn) {
     die("Database connection failed");
